a06849c6bfd6ff7ebdab301746e93f71b233e2dd
Foudroyant <stephanebazebibouta@gmail.com>
2024-02-24 23:36:41 +0100

    Ajout de Adsense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <meta name="google-adsense-account" content="ca-pub-4525015892685820"></meta>
     </html>
   );
 }
